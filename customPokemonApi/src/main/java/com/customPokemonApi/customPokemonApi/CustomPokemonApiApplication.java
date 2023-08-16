d3cc2c204545e441bcbdbdc99ac643b8ec05bb53
Sergio Santiago Sánchez <sergio.santiago@insinno.es>
2023-08-16 13:25:30 +0200

    First commit

---
+++ customPokemonApi/src/main/java/com/customPokemonApi/customPokemonApi/CustomPokemonApiApplication.java
@@ -0,0 +1,13 @@
+package com.customPokemonApi.customPokemonApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomPokemonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomPokemonApiApplication.class, args);
+	}
+
+}
---
+++ customPokemonApi/src/test/java/com/customPokemonApi/customPokemonApi/CustomPokemonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customPokemonApi.customPokemonApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomPokemonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
