3f3a1372e3a684741d3b0fe1f182cb3b9e269fdb
Sergio Santiago Sánchez <sergio.santiago@insinno.es>
2023-08-17 12:12:20 +0200

    Feat: isValidLogin in repo added

--- customPokemonApi/src/main/java/com/customPokemonApi/customPokemonApi/repository/UserRepository.java
+++ customPokemonApi/src/main/java/com/customPokemonApi/customPokemonApi/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.customPokemonApi.customPokemonApi.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.customPokemonApi.customPokemonApi.models.UserModel;
 
 public interface UserRepository extends CrudRepository<UserModel, Integer>  {
-
+	
+	@Query("SELECT COUNT(*) == 1 FROM credentials c WHERE c.mail = ?1 and c.password = ?2")
+	public Boolean isValidLogin(String mail, String password);
 }
